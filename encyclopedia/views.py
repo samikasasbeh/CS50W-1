47c316d7f3ce7398c530e7c93969ce7d8c92a283
Sami Kasasbeh <Sami.Kasasbehdev@gmail.com>
2021-03-12 21:20:15 -0330

    Adding the entry fuctions

--- encyclopedia/views.py
+++ encyclopedia/views.py
@@ -8,3 +8,14 @@ def index(request):
         "entries": util.list_entries()
     })
 
+def entry(request, entry):
+    entrypage= util.get_entry(entry)
+    if entrypage is None:
+        return render(request, "ecnyclopedia/nonExistingEntry.html", {
+            "entryTitle": entry
+        })
+    else:
+        return render(request,"encyclopedia/entry.html", {
+            "entry": entrypage,
+            "entryTitle":entry
+        })
