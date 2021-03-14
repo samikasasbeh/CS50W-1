364b779554de14211c294e14177916c7ed3ce1d9
Sami Kasasbeh <Sami.Kasasbehdev@gmail.com>
2021-03-13 22:24:44 -0330

    adding some comments to make the project clear and starting to work on the search

--- encyclopedia/views.py
+++ encyclopedia/views.py
@@ -1,13 +1,18 @@
 from django.shortcuts import render
 from markdown2 import Markdown
 from . import util
+from django import forms
 
+#adding to create a form in search to manipulate
+class searchForm(forms.Form):
+    query = forms.CharField(max_length=100)
 
+#Default landing page view
 def index(request):
     return render(request, "encyclopedia/index.html", {
         "entries": util.list_entries()
     })
-
+#view to show a specific page the user search in url
 def entry(request, entry):
     markdowner=Markdown()
     entrypage= util.get_entry(entry)
@@ -20,3 +25,6 @@ def entry(request, entry):
             "entry": markdowner.convert(entrypage),
             "entryTitle":entry
         })
+
+# to view the pages the user searched for 
+def search(request)
